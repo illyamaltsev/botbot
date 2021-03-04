dd13805d7cf3fa7744a91b9aaf851f580cb22099
Illya Maltsev <imaltsev98@gmail.com>
2021-03-04 20:50:38 +0200

    add Flask as a server

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 from telebot import TeleBot
+from flask import Flask, request
+from telebot.types import Update
+
+app = Flask(__name__)
+
+
+@app.route("/tg/", methods=['GET', 'POST'])
+def tg():
+    update = Update.de_json(request.stream.read().decode('utf-8'))
+    bot.process_new_updates([update])
+    return 'ok', 200
+
 
 bot = TeleBot("1627794202:AAE0gGxG93vgxbFwRETiUYQvXwmVphDvAOQ", threaded=False)
 
@@ -6,6 +18,3 @@ bot = TeleBot("1627794202:AAE0gGxG93vgxbFwRETiUYQvXwmVphDvAOQ", threaded=False)
 @bot.message_handler(content_types=["text"])
 def handle(msg):
     bot.send_message(msg.from_user.id, msg.text)
-
-
-bot.polling()
